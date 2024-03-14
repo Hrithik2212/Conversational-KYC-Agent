44804a331ae5148c5b2b0a2511062421e5c435ec
Aakash <93485049+Aakash-sittu@users.noreply.github.com>
2024-03-14 17:33:22 +0530

    index and public folder

---
+++ landing_page/constants/index.ts
@@ -0,0 +1,88 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: 'http://localhost:3000', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_we_work', label: 'How We work' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Seamless Verification Anywhere:',
+    icon: '/map.svg',
+    variant: 'blue',
+    description:
+      'Our KYC system ensures uninterrupted identity verification, even in areas with poor or no internet connectivity. With offline verification capabilities, you can confidently use our application anytime, anywhere, without relying on a stable internet connection.',
+  },
+  {
+    title: 'Schedule Verification Sessions:',
+    icon: '/calendar.svg',
+    variant: 'blue',
+    description:
+      "Plan your identity verification sessions with ease. Our system allows you to schedule appointments for KYC verification, ensuring a hassle-free process for you and your associates. Take advantage of our flexible scheduling options to eliminate any scheduling conflicts or delays.",
+  },
+  {
+    title: 'Cutting-edge Biometric Verification:',
+    icon: '/tech.svg',
+    variant: 'blue',
+    description:
+      "Our KYC system utilizes cutting-edge biometric verification technology to securely authenticate your identity. Powered by the latest advancements in biometrics, our system offers a seamless verification experience without the need for an internet connection. Enjoy the convenience and security of biometric authentication wherever you are.",
+  },
+  {
+    title: 'Expanding Coverage:',
+    icon: '/location.svg',
+    variant: 'blue',
+    description:
+      'Access our KYC system to verify your identity across a growing number of locations. Benefit from our expanding network of verification points, fueled by a global community committed to sharing their experiences and insights. Explore new verification opportunities every month as our network continues to grow.',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'something',
+      'something',
+      'something',
+      'Jobs',
+      'Privacy Policy',
+      'Contact Us',
+    ],
+  },
+  {
+    title: 'Our Community',
+    links: ['value1', 'value2', 'value3'],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Reach Us ',
+  links: [
+    { label: 'Hrithik Reddy', value: 'putemail' },
+    { label: 'Aakash', value: 'putemail' },
+    { label: 'Diya Varghese', value: 'putemail' },
+    { label: 'Gopal', value: 'putemail' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Social',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
